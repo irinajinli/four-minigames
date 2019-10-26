11d8e8e78e414bc32c47618de7b99771d2cb7c49
ashley <ash.lu@mail.utoronto.ca>
2019-10-26 16:38:33 -0400

    Made LoginActivity open on launch. Added textboxes and buttons. Login button redirects user to AppleActivity

--- phase1/Game1/app/src/main/java/com/example/game1/presentation/LoginActivity.java
+++ phase1/Game1/app/src/main/java/com/example/game1/presentation/LoginActivity.java
@@ -2,9 +2,13 @@ package com.example.game1.presentation;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 import com.example.game1.R;
+import com.example.game1.presentation.applegame.AppleActivity;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -12,5 +16,18 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    /**
+     * Called when the user taps the Login button
+     */
+    public void sendLoginCredentials(View view) {
+        /* TODO: This method should send the login credentials to another class to validate it.
+            For now, its just redirects the user to a game*/
+        Intent intent = new Intent(this, AppleActivity.class);
+//        EditText editText = (EditText) findViewById(R.id.editText);
+//        String message = editText.getText().toString();
+//        intent.putExtra(EXTRA_MESSAGE, message); // store as many key value pairs in intent as you want
+        startActivity(intent);
     }
 }
