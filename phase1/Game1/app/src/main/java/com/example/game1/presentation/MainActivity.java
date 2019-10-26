cfde1122ab78ed1ceeb11a7b24095dec01b9953d
ashley <ash.lu@mail.utoronto.ca>
2019-10-26 17:05:11 -0400

    Created MainActivity which opens on launch.

---
+++ phase1/Game1/app/src/main/java/com/example/game1/presentation/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.game1.presentation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.game1.R;
+import com.example.game1.presentation.applegame.AppleActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * Called when the user taps the Login button
+     */
+    public void goToLogin(View view) {
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+    }
+
+    /**
+     * Called when the user taps the Register button
+     */
+    public void goToRegistration(View view) {
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+    }
+}
