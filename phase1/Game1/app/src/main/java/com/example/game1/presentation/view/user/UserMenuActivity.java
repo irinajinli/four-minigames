81d19ba282ffc7ecac3f962e3930bf443880846f
ashley <ash.lu@mail.utoronto.ca>
2019-10-28 20:53:35 -0400

    Created UserMenuActivity

---
+++ phase1/Game1/app/src/main/java/com/example/game1/presentation/view/user/UserMenuActivity.java
@@ -0,0 +1,15 @@
+package com.example.game1.presentation.view.user;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import com.example.game1.R;
+
+public class UserMenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_menu);
+    }
+}
