defd337e54a004407d3650bb1daef4ad0861d4bb
lijean <jean.li@mail.utoronto.ca>
2019-10-30 17:25:33 -0400

    Added documentation to PointsCounter.

--- phase1/Game1/app/src/main/java/com/example/game1/presentation/view/applegame/PointsCounter.java
+++ phase1/Game1/app/src/main/java/com/example/game1/presentation/view/applegame/PointsCounter.java
@@ -6,10 +6,13 @@ import android.graphics.Color;
 import com.example.game1.presentation.view.common.GameItem;
 import com.example.game1.presentation.view.common.GameView;
 
+/** A counter for points earned. */
 public class PointsCounter extends GameItem {
 
+  /** How many points have been counted. */
   private int numPoints = 0;
 
+  /** Constructs a PointsCounter with white text. */
   public PointsCounter() {
     super("0");
     paintText.setColor(Color.WHITE);
@@ -20,15 +23,33 @@ public class PointsCounter extends GameItem {
     // TODO: empty method? fix?
   }
 
+  /**
+   * Draws this PointsCounter.
+   *
+   * @param canvas the canvas on which to draw
+   * @param s the String to draw
+   * @param x the x coordinate at which to draw
+   * @param y the y coordinate at which to draw
+   */
   @Override
   public void drawString(Canvas canvas, String s, int x, int y) {
     canvas.drawText(s, x * GameView.charWidth, y * GameView.charHeight, super.paintText);
   }
 
+  /**
+   * Gets numPoints.
+   *
+   * @return
+   */
   public int getNumPoints() {
     return numPoints;
   }
 
+  /**
+   * Adds the specified number of points.
+   *
+   * @param points
+   */
   public void addPoints(int points) {
     numPoints += points;
     setAppearance(numPoints + ""); // concatenate to make String
