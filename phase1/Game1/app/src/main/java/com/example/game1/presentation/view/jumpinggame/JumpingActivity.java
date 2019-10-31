8277d307b2505d9d12fbecc581c8e82869ee62df
Abdul Arif <abdul.arif@mail.utoronto.ca>
2019-10-31 02:18:18 -0400

    push test

---
+++ phase1/Game1/app/src/main/java/com/example/game1/presentation/view/jumpinggame/JumpingActivity.java
@@ -0,0 +1,16 @@
+package com.example.game1.presentation.view.jumpinggame;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.game1.R;
+
+public class JumpingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_jumping);
+    }
+}
