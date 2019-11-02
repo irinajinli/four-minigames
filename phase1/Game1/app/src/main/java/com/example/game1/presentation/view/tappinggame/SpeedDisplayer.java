4e675d95a0771b2c3b3eb1aa6af32364c6b1bde9
Luqiong Xie <luqiong.xie@mail.utoronto.ca>
2019-11-02 02:57:52 -0400

    Adding tapping game SpeedDisplayer.java and StarDisplayer.java

--- phase1/Game1/app/src/main/java/com/example/game1/presentation/view/tappinggame/SpeedDisplayer.java
+++ phase1/Game1/app/src/main/java/com/example/game1/presentation/view/tappinggame/SpeedDisplayer.java
@@ -1,4 +1,28 @@
 package com.example.game1.presentation.view.tappinggame;
 
-public class SpeedDisplayer {
+import android.graphics.Color;
+
+public class SpeedDisplayer extends TappingItem {
+    /** construct a Tap Counter at the specified cursor location (xCoordinate, yCoordinate). */
+    private int speed;
+
+    public SpeedDisplayer(int xCoordinate, int yCoordinate) {
+        // Call super() to set appearance, location (xCoordinate, yCoordinate), appearance and
+        // type face.
+        super("Your average tapping speed: ", xCoordinate, yCoordinate);
+        this.speed = 0;
+
+        paintText.setColor(Color.CYAN);
+    }
+
+    public int getSpeed(){
+        return speed;
+    }
+
+
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+        this.appearance = "Your average tapping speed: " + this.speed;
+    }
 }
--- phase1/Game1/app/src/main/java/com/example/game1/presentation/view/tappinggame/StarDisplayer.java
+++ phase1/Game1/app/src/main/java/com/example/game1/presentation/view/tappinggame/StarDisplayer.java
@@ -1,4 +1,28 @@
 package com.example.game1.presentation.view.tappinggame;
 
-public class StarDisplayer {
+import android.graphics.Color;
+
+public class StarDisplayer extends TappingItem {
+    /** construct a Tap Counter at the specified cursor location (xCoordinate, yCoordinate). */
+    private int numStar;
+
+    public StarDisplayer(int xCoordinate, int yCoordinate) {
+        // Call super() to set appearance, location (xCoordinate, yCoordinate), appearance and
+        // type face.
+        super("You current star number: ", xCoordinate, yCoordinate);
+        this.numStar = 0;
+
+        paintText.setColor(Color.CYAN);
+    }
+
+    public int getNumStar(){
+        return numStar;
+    }
+
+
+
+    public void setNumStar(int numStar) {
+        this.numStar = numStar;
+        this.appearance = "You current star number: " + this.numStar;
+    }
 }
