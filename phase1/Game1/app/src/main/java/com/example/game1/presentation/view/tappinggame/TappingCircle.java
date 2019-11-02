5c4ae553b0874cef1b1908baede1fe071d1a7f36
Luqiong Xie <luqiong.xie@mail.utoronto.ca>
2019-11-02 02:54:19 -0400

    Changing TappingCircle.java

--- phase1/Game1/app/src/main/java/com/example/game1/presentation/view/tappinggame/TappingCircle.java
+++ phase1/Game1/app/src/main/java/com/example/game1/presentation/view/tappinggame/TappingCircle.java
@@ -1,13 +1,39 @@
 package com.example.game1.presentation.view.tappinggame;
 
 import android.graphics.Bitmap;
+import android.graphics.Matrix;
 
-public class TappingCircle extends ImageGameItem {
+/** a tapping circle */
+public class TappingCircle extends TappingItem {
+    /** construct a tapping circle at the specified cursor location (xCoordinate, yCoordinate). */
+    public TappingCircle(Bitmap appearance, int xCoordinate, int yCoordinate) {
+        // Call super() to set appearance, location (xCoordinate, yCoordinate), appearance and
+        // type face.
+        super(appearance, xCoordinate, yCoordinate);
+        this.appearance = getResizedBitmap(appearance, 1000, 1000);
+    }
 
-    public TappingCircle(Bitmap imageBMP, int width, int height, int xCoordinate, int yCoordinate){
-        super(imageBMP, width, height, xCoordinate, yCoordinate);
-    };
+    /**
+     * code taken from: https://stackoverflow.com/questions/4837715/how-to-resize-a-bitmap-in-android
+     *
+     * @param bm
+     * @param newWidth
+     * @param newHeight
+     * @return
+     */
+    public Bitmap getResizedBitmap(Bitmap bm, int newWidth, int newHeight) {
+        int width = bm.getWidth();
+        int height = bm.getHeight();
+        float scaleWidth = ((float) newWidth) / width;
+        float scaleHeight = ((float) newHeight) / height;
+        // CREATE A MATRIX FOR THE MANIPULATION
+        Matrix matrix = new Matrix();
+        // RESIZE THE BIT MAP
+        matrix.postScale(scaleWidth, scaleHeight);
 
-
-
+        // "RECREATE" THE NEW BITMAP
+        Bitmap resizedBitmap = Bitmap.createBitmap(bm, 0, 0, width, height, matrix, false);
+        bm.recycle();
+        return resizedBitmap;
+    }
 }
