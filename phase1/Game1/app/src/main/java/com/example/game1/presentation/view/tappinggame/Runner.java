1f647f3d6353a7f59357b26071212b341341de9f
Luqiong Xie <luqiong.xie@mail.utoronto.ca>
2019-11-02 02:51:41 -0400

    Changing Tapping Game classes

--- phase1/Game1/app/src/main/java/com/example/game1/presentation/view/tappinggame/Runner.java
+++ phase1/Game1/app/src/main/java/com/example/game1/presentation/view/tappinggame/Runner.java
@@ -1,13 +1,41 @@
 package com.example.game1.presentation.view.tappinggame;
 
 import android.graphics.Bitmap;
+import android.graphics.Matrix;
 
-public class Runner extends ImageGameItem {
-    public Runner(Bitmap imageBMP, int width, int height, int xCoordinate, int yCoordinate){
-        super(imageBMP, width, height, xCoordinate, yCoordinate);
+/**
+ * Runner of the tapping game
+ */
+public class Runner extends TappingItem {
+    /**
+     * Constructs a runner at the specified cursor location (xCoordinate, yCoordinate).
+     */
+    public Runner(Bitmap appearance, int xCoordinate, int yCoordinate) {
+        super(appearance, xCoordinate, yCoordinate);
+        this.appearance = getResizedBitmap(appearance, 100, 100);
     }
 
-    public void run(int speed){
-        this.xCoordinate += speed;
+    /**
+     * code taken from: https://stackoverflow.com/questions/4837715/how-to-resize-a-bitmap-in-android
+     *
+     * @param bm
+     * @param newWidth
+     * @param newHeight
+     * @return
+     */
+    public Bitmap getResizedBitmap(Bitmap bm, int newWidth, int newHeight) {
+        int width = bm.getWidth();
+        int height = bm.getHeight();
+        float scaleWidth = ((float) newWidth) / width;
+        float scaleHeight = ((float) newHeight) / height;
+        // CREATE A MATRIX FOR THE MANIPULATION
+        Matrix matrix = new Matrix();
+        // RESIZE THE BIT MAP
+        matrix.postScale(scaleWidth, scaleHeight);
+
+        // "RECREATE" THE NEW BITMAP
+        Bitmap resizedBitmap = Bitmap.createBitmap(bm, 0, 0, width, height, matrix, false);
+        bm.recycle();
+        return resizedBitmap;
     }
 }
