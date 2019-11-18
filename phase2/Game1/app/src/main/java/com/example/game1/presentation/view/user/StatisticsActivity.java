71f28107aaee087304c23687afba4fdf7d3206d4
ashley <ash.lu@mail.utoronto.ca>
2019-11-17 20:11:23 -0500

    Edited StatisticsActivity.

--- phase2/Game1/app/src/main/java/com/example/game1/presentation/view/user/StatisticsActivity.java
+++ phase2/Game1/app/src/main/java/com/example/game1/presentation/view/user/StatisticsActivity.java
@@ -21,7 +21,7 @@ public class StatisticsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_statistics);
 
         EditText infoText = findViewById(R.id.infoText);
-        infoText.setText("Your score = points + stars + taps");
+        infoText.setText("score = points + stars + taps");
 
         /*
         Display the current score of the current user
