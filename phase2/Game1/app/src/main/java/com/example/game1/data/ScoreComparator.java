bb7a10f028228ab243a9ce6e910f68cb4d3824d0
ashley <ash.lu@mail.utoronto.ca>
2019-11-30 13:44:44 -0500

    Condensed comments in ScoreComparator, StarsComparator, and TapsComparator

--- phase2/Game1/app/src/main/java/com/example/game1/data/ScoreComparator.java
+++ phase2/Game1/app/src/main/java/com/example/game1/data/ScoreComparator.java
@@ -10,11 +10,6 @@ public class ScoreComparator implements Comparator<User> {
    *
    * <p>Returns a negative integer, zero, or a positive integer as user1 is less than, equal to, or
    * greater than user2 in terms of the top score in their history.
-   *
-   * @param user1 the first User to compare
-   * @param user2 the second User to compare
-   * @return a negative integer, zero, or a positive integer as user1 is less than, equal to, or
-   *     greater than user2
    */
   @Override
   public int compare(User user1, User user2) {
--- phase2/Game1/app/src/main/java/com/example/game1/data/StarsComparator.java
+++ phase2/Game1/app/src/main/java/com/example/game1/data/StarsComparator.java
@@ -5,17 +5,11 @@ import com.example.game1.presentation.model.User;
 import java.util.Comparator;
 
 public class StarsComparator implements Comparator<User> {
-
   /**
    * Compares its two arguments for order.
    *
    * <p>Returns a negative integer, zero, or a positive integer as user1 is less than, equal to, or
    * greater than user2 in terms of the top number of stars in their history.
-   *
-   * @param user1 the first User to compare
-   * @param user2 the second User to compare
-   * @return a negative integer, zero, or a positive integer as user1 is less than, equal to, or
-   *     greater than user2
    */
   @Override
   public int compare(User user1, User user2) {
