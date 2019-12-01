622ddd4a0855fb063344327e01583163b17fc181
Luqiong Xie <luqiong.xie@mail.utoronto.ca>
2019-12-01 16:02:57 -0500

    Java doc for TappingCircle class

--- phase2/Game1/app/src/main/java/com/example/game1/presentation/model/tappinggame/TappingMovementInfo.java
+++ phase2/Game1/app/src/main/java/com/example/game1/presentation/model/tappinggame/TappingMovementInfo.java
@@ -1,44 +1,66 @@
 package com.example.game1.presentation.model.tappinggame;
 
-
 import com.example.game1.presentation.model.common.MovementInfo;
 
-
+/** A class for passing information for tapping game items to process update. */
 public class TappingMovementInfo extends MovementInfo {
+  /** Napping Speed */
+  int tappingSpeed;
 
-    int tappingSpeed;
+  /** Number of seconds left */
+  int secondsLeft;
 
+  /** Number of taps */
+  int numTaps;
 
+  /**
+   * Construct TappingMovementInfo to store informaton needed for tapping game item to perform
+   * update
+   *
+   * @param screenHeight height of the screen
+   * @param screenWidth width of the screen
+   * @param tappingSpeed tapping speed
+   * @param secondsLeft number of seconds left
+   * @param numTaps number of taps
+   * @param numSeconds time period for updating coordinates
+   */
+  public TappingMovementInfo(
+      int screenHeight,
+      int screenWidth,
+      int tappingSpeed,
+      int secondsLeft,
+      int numTaps,
+      double numSeconds) {
+    super(numSeconds, screenHeight, screenWidth);
+    this.tappingSpeed = tappingSpeed;
+    this.secondsLeft = secondsLeft;
+    this.numTaps = numTaps;
+  }
 
-    int secondsLeft;
+  /**
+   * Return the tapping speed
+   *
+   * @return the tapping speed
+   */
+  public int getTappingSpeed() {
+    return tappingSpeed;
+  }
 
-    int numTaps;
-    public TappingMovementInfo(
-            int screenHeight, int screenWidth, int tappingSpeed, int secondsLeft, int numTaps, double numSeconds) {
+  /**
+   * Return the number of taps
+   *
+   * @return
+   */
+  public int getNumTaps() {
+    return numTaps;
+  }
 
-        super(numSeconds, screenHeight, screenWidth);
-
-        this.tappingSpeed = tappingSpeed;
-        this.secondsLeft = secondsLeft;
-        this.numTaps = numTaps;
-    }
-
-    public int getTappingSpeed() {
-
-        return tappingSpeed;
-    }
-
-    public int getNumTaps() {
-        return numTaps;
-    }
-
-    public void setNumTaps(int numTaps) {
-        this.numTaps = numTaps;
-    }
-
-    public int getSecondsLeft() {
-        return secondsLeft;
-    }
-
-
+  /**
+   * Return the number of seconds left
+   *
+   * @return
+   */
+  public int getSecondsLeft() {
+    return secondsLeft;
+  }
 }
