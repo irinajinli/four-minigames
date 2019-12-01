db98888ceda7435e497713a5aa3af319d118203a
Luqiong Xie <luqiong.xie@mail.utoronto.ca>
2019-12-01 12:29:19 -0500

    adding java doc and comments for Result class

--- phase2/Game1/app/src/main/java/com/example/game1/presentation/model/common/Result.java
+++ phase2/Game1/app/src/main/java/com/example/game1/presentation/model/common/Result.java
@@ -1,42 +1,67 @@
 package com.example.game1.presentation.model.common;
 
-import com.example.game1.presentation.model.common.GameItem;
-
 import java.util.ArrayList;
 import java.util.List;
 
 public class Result {
-    /** A class to store infromation that is needed for game manager to update the list */
+  /** A class to store infromation that is needed for game manager to update the list */
 
-    /** Items to be added to the list */
-    List<GameItem> newItems = new ArrayList<>();
-    /** Items to be removed from the list */
-    List<GameItem> oldItems = new ArrayList<>();
+  /** Items to be added to the list */
+  List<GameItem> newItems = new ArrayList<>();
+  /** Items to be removed from the list */
+  List<GameItem> oldItems = new ArrayList<>();
 
-    public List<GameItem> getNewItems() {
-        return newItems;
-    }
+  /**
+   * Set the list of game items that are to be added
+   *
+   * @param newItems the list of game items that are to be added
+   */
+  public void setNewItems(List<GameItem> newItems) {
+    this.newItems = newItems;
+  }
 
-    public void setNewItems(List<GameItem> newItems) {
-        this.newItems = newItems;
-    }
+  /**
+   * Return the List of game items that are to be added
+   *
+   * @return the List of game items that are to be added
+   */
+  public List<GameItem> getNewItems() {
+    return newItems;
+  }
 
-    public void addNewItem(GameItem item) {
-        this.newItems.add(item);
-    }
+  /**
+   * Add a new game item to the newItems list so that it will be added later
+   *
+   * @param item add a game item to the newItems list so that it will be added later
+   */
+  public void addNewItem(GameItem item) {
+    this.newItems.add(item);
+  }
 
-    public List<GameItem> getOldItems() {
-        return oldItems;
-    }
+  /**
+   * Set the list of game items that are to be removed
+   *
+   * @param oldItems the list of game items that are to be removed
+   */
+  public void setOldItems(List<GameItem> oldItems) {
+    this.oldItems = oldItems;
+  }
 
-    public void setOldItems(List<GameItem> oldItems) {
-        this.oldItems = oldItems;
-    }
+  /**
+   * Return the List of game items that are to be removed
+   *
+   * @return the List of game items that are to be removed
+   */
+  public List<GameItem> getOldItems() {
+    return oldItems;
+  }
 
-    public void addOldItem(GameItem item) {
-        this.oldItems.add(item);
-    }
-
-
-
+  /**
+   * Add a new game item to the oldItems list so that it will be removed later
+   *
+   * @param item add a new game item to the oldItems list so that it will be removed later
+   */
+  public void addOldItem(GameItem item) {
+    this.oldItems.add(item);
+  }
 }
