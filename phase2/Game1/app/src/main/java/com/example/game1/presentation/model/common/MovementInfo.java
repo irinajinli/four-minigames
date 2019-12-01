c521d4581cd7781b4c99a362be330d661b5b223f
Luqiong Xie <luqiong.xie@mail.utoronto.ca>
2019-12-01 12:20:15 -0500

    Adding java doc for MovementInfo class

--- phase2/Game1/app/src/main/java/com/example/game1/presentation/model/common/MovementInfo.java
+++ phase2/Game1/app/src/main/java/com/example/game1/presentation/model/common/MovementInfo.java
@@ -1,32 +1,66 @@
 package com.example.game1.presentation.model.common;
 
-
 public class MovementInfo {
-    private double numSeconds;
-    private int screenHeight, screenWidth;
+  /**
+   * A period of time In this game it is the number of seconds used to refresh new xCoordinate,
+   * yCoordinate, xVelocity, * yVelocity based on current xAcceleration and yAcceleration. Currently
+   * numOfSeconds = * GameThread.FRAME_DURATION_NS / 1000000000.
+   */
+  private double numSeconds;
 
+  /** the height of the screen */
+  private int screenHeight;
 
-    public MovementInfo(double numSeconds, int screenHeight, int screenWidth){
-        this.numSeconds = numSeconds;
-        this.screenHeight = screenHeight;
-        this.screenWidth = screenWidth;
-    }
+  /** the width of the screen */
+  private int screenWidth;
 
-    public int getScreenHeight(){
-        return screenHeight;
-    }
+  /**
+   * Construct the MovementInfo to store information needed for a game item to perform update()
+   *
+   * @param numSeconds
+   * @param screenHeight
+   * @param screenWidth
+   */
+  public MovementInfo(double numSeconds, int screenHeight, int screenWidth) {
+    this.numSeconds = numSeconds;
+    this.screenHeight = screenHeight;
+    this.screenWidth = screenWidth;
+  }
 
-    public int getScreenWidth(){
-        return screenWidth;
-    }
+  /**
+   * Return the height of the current screen
+   *
+   * @return the height of the current screen
+   */
+  public int getScreenHeight() {
+    return screenHeight;
+  }
 
-    public double getNumSeconds() {
-        return numSeconds;
-    }
+  /**
+   * Return the width of the current screen
+   *
+   * @return the width of the current screen
+   */
+  public int getScreenWidth() {
+    return screenWidth;
+  }
 
-    public void setNumSeconds(double numSeconds) {
-        this.numSeconds = numSeconds;
-    }
+  /**
+   * Return the number of seconds needed for coordinates update
+   *
+   * @return the number of seconds needed for coordinates update
+   */
+  public double getNumSeconds() {
+    return numSeconds;
+  }
 
-
+  /**
+   * Set the number of seconds needed for coordinates update
+   * This
+   *
+   * @param numSeconds the time period needed for coordinates update
+   */
+  public void setNumSeconds(double numSeconds) {
+    this.numSeconds = numSeconds;
+  }
 }
