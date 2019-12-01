56c52282c71700b003e63cff813783888e35a016
Luqiong Xie <luqiong.xie@mail.utoronto.ca>
2019-12-01 15:20:32 -0500

    Java doc for SpeedDisplayer class

--- phase2/Game1/app/src/main/java/com/example/game1/presentation/model/tappinggame/SpeedDisplayer.java
+++ phase2/Game1/app/src/main/java/com/example/game1/presentation/model/tappinggame/SpeedDisplayer.java
@@ -4,30 +4,34 @@ import com.example.game1.presentation.model.common.GameItem;
 import com.example.game1.presentation.model.common.MovementInfo;
 import com.example.game1.presentation.model.common.Result;
 
+/** A SpeedDisplayer */
 public class SpeedDisplayer extends GameItem {
-  /** construct a Tap Counter at the specified cursor location (xCoordinate, yCoordinate). */
+  /** the tapping speed */
   private int tappingSpeed;
 
+  /**
+   * construct a speedDisplayer at the specified cursor location (xCoordinate, yCoordinate
+   *
+   * @param xCoordinate
+   * @param yCoordinate
+   */
   public SpeedDisplayer(double xCoordinate, double yCoordinate) {
-    // Call super() to set location (x, y)
     super(xCoordinate, yCoordinate, "Your average tapping speed: ");
     this.tappingSpeed = 0;
   }
 
-//  public int getSpeed() {
-//    return tappingSpeed;
-//  }
-
-//  public void setSpeed(int speed) {
-//    this.tappingSpeed = speed;
-//    setDescription("Your average tapping speed: " + this.tappingSpeed);
-//  }
-
   @Override
+  /**
+   * Perform update based on the information given by the movement info
+   *
+   * @param tappingMovementInfo
+   * @return result needed by the tapping game manager.
+   */
   public Result update(MovementInfo tappingMovementInfo) {
-    //TappingResult result = new TappingResult();
     if (tappingMovementInfo instanceof TappingMovementInfo) {
+      // Extract tapping speed for tapping movement info
       this.tappingSpeed = ((TappingMovementInfo) tappingMovementInfo).getTappingSpeed();
+      // Update description
       setDescription("Your average tapping speed: " + this.tappingSpeed);
     }
     return (new Result());
