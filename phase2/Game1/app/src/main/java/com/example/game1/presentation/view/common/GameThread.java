d932c5cb293fdb1726391c54ef6659bd3baf641f
lijean <jean.li@mail.utoronto.ca>
2019-12-01 22:58:36 -0500

    Updated GameThread doc.

--- phase2/Game1/app/src/main/java/com/example/game1/presentation/view/common/GameThread.java
+++ phase2/Game1/app/src/main/java/com/example/game1/presentation/view/common/GameThread.java
@@ -3,21 +3,33 @@ package com.example.game1.presentation.view.common;
 import android.graphics.Canvas;
 import android.view.SurfaceHolder;
 
+/** A thread of execution. */
 public class GameThread extends Thread {
+  /** The target FPS. */
   private static final long TARGET_FPS = 30;
+  /** The frame duration. */
   public static final long FRAME_DURATION_NS = (long) (Math.pow(10, 9) / TARGET_FPS);
+  /** The SurfaceHolder. */
   private SurfaceHolder surfaceHolder;
+  /** The GameView. */
   private GameView gameView;
+  /** Whether the thread is running. */
   private boolean isRunning;
+  /** The Canvas. */
   private Canvas canvas;
-  // public static final long FRAME_DURATION_NS = 300 * 1000000;
 
+  /**
+   * Constructs a GameThread with the specified SurfaceHolder and GameView.
+   * @param surfaceHolder the SurfaceHolder
+   * @param gameView the GameView
+   */
   public GameThread(SurfaceHolder surfaceHolder, GameView gameView) {
     super();
     this.surfaceHolder = surfaceHolder;
     this.gameView = gameView;
   }
 
+  /** Runs this Thread. */
   @Override
   public void run() {
     canvas = null;
@@ -68,6 +80,9 @@ public class GameThread extends Thread {
     }
   }
 
+  /** Sets isRunning.
+   * @param isRunning isRunning
+   */
   public void setRunning(boolean isRunning) {
     this.isRunning = isRunning;
   }
